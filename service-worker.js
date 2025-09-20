c56aca33269aee7bfcc09ccc86f35bc24ed2a98d
debor291-afk <debor291@gmail.com>
2025-09-20 15:54:27 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-const CACHE_NAME='unfold-collage-v1';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('fetch',e=>{});
